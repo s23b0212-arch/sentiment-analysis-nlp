eaabedd582b00f6b0ea82760ec92aa43d0dd081e
s23b0212-arch <s23b0212@siswa.umk.edu.my>
2026-07-04 06:42:57 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,34 @@
 import streamlit as st
+import pickle
+import re
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
 
-st.title("Sentiment Analysis App")
+model = pickle.load(open("sentiment_model.pkl", "rb"))
+tfidf = pickle.load(open("tfidf.pkl", "rb"))
+
+stop_words = set(stopwords.words('english'))
+lemmatizer = WordNetLemmatizer()
+
+def clean_text(text):
+    text = str(text).lower()
+    text = re.sub(r'[^a-zA-Z]', ' ', text)
+    words = text.split()
+    words = [w for w in words if w not in stop_words]
+    words = [lemmatizer.lemmatize(w) for w in words]
+    return " ".join(words)
+
+st.title("🎬 IMDb Sentiment Analysis")
+
+text = st.text_area("Enter movie review")
+
+if st.button("Predict"):
+    cleaned = clean_text(text)
+    vector = tfidf.transform([cleaned])
+    pred = model.predict(vector)
+
+    if pred[0] == 1:
+        st.success("Positive 😊")
+    else:
+        st.error("Negative 😡")
