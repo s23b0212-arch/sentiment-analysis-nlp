c8d0338e372497e1f5dbaa7b51ec817c20751e45
s23b0212-arch <s23b0212@siswa.umk.edu.my>
2026-07-04 06:16:39 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,3 @@
+import streamlit as st
 
-git init
-git add .
-git commit -m "NLP Sentiment Analysis Project"
-git branch -M main
-git remote add origin https://github.com/your-username/your-repo.git
-git push -u origin main
+st.title("Sentiment Analysis App")
