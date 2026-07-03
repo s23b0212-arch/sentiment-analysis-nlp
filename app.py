5645506330f83eb4a05724605f793b099aaa2bbd
s23b0212-arch <s23b0212@siswa.umk.edu.my>
2026-07-04 07:54:59 +0800

    Update app.py

--- app.py
+++ app.py
@@ -122,19 +122,7 @@ elif page == "🧠 Predict":
         "✍️ Custom": ""
     }
 
-    choice = st.selectbox("Choose review type:", list(samples.keys()))
-
-    if "review" not in st.session_state:
-        st.session_state.review = ""
-
-    if choice != "✍️ Custom":
-        st.session_state.review = samples[choice]
-
-    review = st.text_area(
-        "Enter your review:",
-        value=st.session_state.review,
-        height=120
-    )
+    
 
     st.session_state.review = review
 
