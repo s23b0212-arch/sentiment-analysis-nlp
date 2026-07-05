b6f7b415f717c25a451389949bc7552ab276fa75
s23b0212-arch <s23b0212@siswa.umk.edu.my>
2026-07-05 21:19:45 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-from wordcloud import WordCloud
+
 import plotly.express as px
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
